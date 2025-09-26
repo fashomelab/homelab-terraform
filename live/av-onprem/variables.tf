e91237ab9708022e4e0f19be61ebc7190b58950b
hades <cblignaut989@hotmail.com>
2025-09-26 07:29:19 +0100

    Updates to workflow

--- live/av-onprem/variables.tf
+++ live/av-onprem/variables.tf
@@ -1,7 +1,1 @@
-# labsterraform/environments/fashomelab/variables.tf
-
-variable "common_tags" {
-  description = "A list of common tags to apply to all VMs in this environment."
-  type        = list(string)
-  default     = []
-}
+# live/av-onprem/variables.tf
